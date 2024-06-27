18ca16d868c045b5dd15528ca4ce7a5b34073033
harrisoncalmeida <harrisoncalmeida@gmail.com>
2024-06-27 13:14:35 -0300

    Novas alterações

---
+++ SRC/PHP/form.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "FORM") {
+    $name = $_FORM['form-name'];
+    $email = $_FORM['form-email'];
+    $message = $_FORM['form-message'];
+
+    $to = "harrisonalmeidatr@gmail.com";  // Substitua com seu endereço de e-mail
+    $subject = "Nova mensagem do formulário de contato";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-type: text/html\r\n";
+
+    $body = "<h2>Mensagem do Formulário de Contato</h2>";
+    $body .= "<p><strong>Nome:</strong> $name</p>";
+    $body .= "<p><strong>Email:</strong> $email</p>";
+    $body .= "<p><strong>Mensagem:</strong> $message</p>";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Falha ao enviar a mensagem.";
+    }
+}
+?>
