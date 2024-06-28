45e1df9a5f485c78861fa2b5ff2edc2278957dfe
harrisoncalmeida <harrisoncalmeida@gmail.com>
2024-06-28 12:36:46 -0300

    Novas alterações

--- SRC/PHP/email.php
+++ SRC/PHP/email.php
@@ -29,5 +29,5 @@
   //Enviar
   mail($destino, $assunto, $arquivo, $headers);
   
-  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+  echo "<meta http-equiv='refresh' content='10;URL=../contact.html'>";
 ?>
